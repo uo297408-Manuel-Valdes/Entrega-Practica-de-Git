451852f031d3130bc4c6178fa7932649ff823562
Manuel Valdés García <psvit@192.168.56.1>
2026-02-15 21:36:59 +0100

    Java modificado del master para el Ej1

--- EntregaUsoGit/src/test/HolaMundo.java
+++ EntregaUsoGit/src/test/HolaMundo.java
@@ -3,10 +3,9 @@ package test;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Buenos dias");
 		System.out.println("Hola Mundo");
 
-		//^^^^ Sitio reservado para el print de la rama alternativa.
-		System.out.println("Hola otra vez desde master");
+		
 	}
 }
