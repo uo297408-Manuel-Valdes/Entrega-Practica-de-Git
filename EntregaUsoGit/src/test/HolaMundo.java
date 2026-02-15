0c030734e728676903a456860f432eb972540c28
Manuel Valdés García <psvit@192.168.56.1>
2026-02-15 21:24:24 +0100

    Java modificado del master para el Ej1

--- EntregaUsoGit/src/test/HolaMundo.java
+++ EntregaUsoGit/src/test/HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
 		
 		System.out.println("Hola Mundo");
 
-		//^^^^ Sitio reservado para el print de la rama alternativa
+		//^^^^ Sitio reservado para el print de la rama alternativa.
 		System.out.println("Hola otra vez desde master");
 	}
 }
