6db8998e552fa146ea7e29624bb6ccb82505c951
psvit <psvit@192.168.56.1>
2026-02-15 21:11:55 +0100

    Java modificado del master para el Ej1

--- EntregaUsoGit/src/test/HolaMundo.java
+++ EntregaUsoGit/src/test/HolaMundo.java
@@ -5,7 +5,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola Mundo");
-		
+
+		//^^^^ Sitio reservado para el print de la rama alternativa
 		System.out.println("Hola otra vez desde master");
 	}
 }
