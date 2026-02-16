71faaa25c8efc9e84f692016fa2c13c888181ab3
Manuel Valdés García <psvit@192.168.56.1>
2026-02-16 10:41:52 +0100

    Pre ej3

--- EntregaUsoGit/src/test/HolaMundo.java
+++ EntregaUsoGit/src/test/HolaMundo.java
@@ -3,7 +3,7 @@ package test;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Feliz Navidad");
+		System.out.println("Buenas noches");
 		System.out.println("Hola Mundo");
 		System.out.println("Hola desde la rama master");
 	}
