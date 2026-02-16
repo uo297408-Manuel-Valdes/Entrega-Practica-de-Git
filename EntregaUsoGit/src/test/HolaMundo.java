eac2fd37a0deedf6f39e9c128e3811fddef44c99
Manuel Valdés García <psvit@192.168.56.1>
2026-02-16 10:15:20 +0100

    Nueva rama para el Ej3

--- EntregaUsoGit/src/test/HolaMundo.java
+++ EntregaUsoGit/src/test/HolaMundo.java
@@ -5,6 +5,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Buenas noches");
 		System.out.println("Hola Mundo");
-		System.out.println("Hola desde la rama master");
+		System.out.println("Hola desde la rama Ejercicio3");
 	}
 }
